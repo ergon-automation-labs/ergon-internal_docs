2f602564b90b47e0f901f2a5ca9b6967ab210eb9
Abby Malson <august.malson@gmail.com>
2026-05-18 07:16:00 -0600

    bump: bot_army_internal_docs incorporates runtime 0.14.19 (heartbeat UUID fix)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BotArmyInternalDocs.MixProject do
   def project do
     [
       app: :bot_army_internal_docs,
-      version: "0.3.17",
+      version: "0.3.18",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
